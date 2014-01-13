d43d0ab3184174446110f8c33c53b82406bf75c5
Chris Dzombak <dzombak@nytimes.com>
2014-01-13 16:02:55 -0500

    Initial commit of skeleton xcode project

---
+++ thingshub/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  thingshub
+//
+//  Created by Chris Dzombak on 1/13/14.
+//  Copyright (c) 2014 Chris Dzombak. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
