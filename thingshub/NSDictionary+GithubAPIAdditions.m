4f0f6afd162a84ab59deec0637af8a00a9b8be28
Chris Dzombak <dzombak@nytimes.com>
2014-01-19 00:34:37 -0500

    Add some methods to NSDictionary+GithubAPIAdditions

--- thingshub/NSDictionary+GithubAPIAdditions.m
+++ thingshub/NSDictionary+GithubAPIAdditions.m
@@ -6,8 +6,33 @@
 //  Copyright (c) 2014 Chris Dzombak. All rights reserved.
 //
 
+#import <OctoKit/OctoKit.h>
 #import "NSDictionary+GithubAPIAdditions.h"
 
 @implementation NSDictionary (GithubAPIAdditions)
+
+- (NSInteger)cdz_issueNumber {
+    NSNumber *number = self[@"number"];
+    return [number integerValue];
+}
+
+- (BOOL)cdz_issueIsOpen {
+    NSString *state = self[@"state"];
+    return [state isEqualToString:@"open"];
+}
+
+- (NSString *)cdz_issueTitle {
+    return self[@"title"] ?: @"";
+}
+
+- (NSString *)cdz_issueDescription {
+    return self[@"description"] ?: @"";
+}
+
+- (NSDate *)cdz_issueDueDate {
+    NSString *dateString = self[@"due_on"];
+    NSValueTransformer *dateTransformer = [NSValueTransformer valueTransformerForName:OCTDateValueTransformerName];
+    return [dateTransformer transformedValue:dateString];
+}
 
 @end
